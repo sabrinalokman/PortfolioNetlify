d0ee3b7d75d1bf913b81db455ad2daff5a7842b8
Sabrina Lokman <35489261+sabrinalokman@users.noreply.github.com>
2020-11-15 02:45:22 -0500

    Update Contact.js

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -4,14 +4,14 @@ const Contact = ({ data }) => {
    const [url, setUrl] = useState('mailto:test@example.com?subject=subject&body=body');
    const [name, setName] = useState('');
    const [subject, setSubject] = useState('');
-   const [email, setEmail] = useState('');
+   const email = data?.email;
    const [message, setMessage] = useState('');
 
    console.log(data)
 
     const handleClick = (e) => {
        e.preventDefault();
-      window.open(`mailto:${email}?subject=${subject}&body=${name}: ${message}`);
+      window.open(`mailto:${email}?subject=${subject}&body=$ ${message}`);
     }
     
 
@@ -43,11 +43,6 @@ const Contact = ({ data }) => {
                   <div>
 						   <label htmlFor="contactName">Name <span className="required">*</span></label>
 						   <input value={name} type="text" defaultValue="" size="35" id="contactName" name="contactName" onChange={e => setName(e.target.value)}/>
-                  </div>
-
-                  <div>
-						   <label htmlFor="contactEmail">Email <span className="required">*</span></label>
-						   <input value={email} type="text" defaultValue="" size="35" id="contactEmail" name="contactEmail" onChange={e=> setEmail(e.target.value)}/>
                   </div>
 
                   <div>
